80122a37b0a325e9e44a87dad8c3f1cf56ec2e79
Hassn <87209355+Hassn11q@users.noreply.github.com>
2024-06-15 01:00:37 +0300

    Update app.py

--- langchain-app/app.py
+++ langchain-app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from langserve import add_routes
 import uvicorn
 from chain import chain
@@ -6,11 +7,21 @@ from chain import chain
 # initiate the app
 app = FastAPI(
     title="LangChain Server",
-    version = "1.0" , 
+    version="1.0",
     description="LangChain Server for FastAPI"
 )
+
+# CORS settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # add routes
-add_routes(app , chain , path= "/chatbot")
+add_routes(app, chain, path="/chatbot")
 
 # run the app
 if __name__ == "__main__":
